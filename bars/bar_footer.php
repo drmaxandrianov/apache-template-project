86d09dcbed97990c624511425c2f87fd854e12c5
Maxim Pestun <max.pestun@gmail.com>
2013-02-13 12:02:19 +0400

    updated anc cleaned

--- bars/bar_footer.php
+++ bars/bar_footer.php
@@ -1,21 +1,19 @@
-        <hr>
+        <!--hr>
         
-        <footer>
-        <p>&copy; PixelOwner 2012</p>
-        </footer>
+        <!--footer>
+        <p>&copy; Name is here</p>
+        </footer-->
         
     </div> <!-- /container -->
     
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
     
-    <script src="js/libs/bootstrap/bootstrap.min.js"></script>
-    
-    <script src="js/plugins.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
     
     <!-- Begin of Google Analytics Code -->
-    <script type="text/javascript">
+    <!--script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-26839033-3']);
         _gaq.push(['_trackPageview']);
@@ -27,6 +25,22 @@
         })();
     </script>
     <!-- End of Google Analytics Code -->
-
+	
+	<!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap/transition.js"></script>
+    <script src="js/bootstrap/alert.js"></script>
+    <script src="js/bootstrap/modal.js"></script>
+    <script src="js/bootstrap/dropdown.js"></script>
+    <script src="js/bootstrap/scrollspy.js"></script>
+    <script src="js/bootstrap/tab.js"></script>
+    <script src="js/bootstrap/tooltip.js"></script>
+	<script src="js/bootstrap/button.js"></script>
+    <script src="js/bootstrap/popovermenu.js"></script>
+    <script src="js/bootstrap/collapse.js"></script>
+    <script src="js/bootstrap/carousel.js"></script>
+    <script src="js/bootstrap/typeahead.js"></script>
+	
 </body>
 </html>
--- bars/bar_header.php
+++ bars/bar_header.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>Global dislike button</title>
+	<title>GMTool Extension</title>
     <meta name="description" content="">
     <meta name="author" content="">
 
@@ -17,39 +17,37 @@
 
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<style>
-    	body {
-    	  padding-top: 60px;
-    	  padding-bottom: 40px;
-    	}
+		body {
+			padding-top: 60px;
+			padding-bottom: 40px;
+		}
 	</style>
 	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
 	<link rel="stylesheet" href="css/style.css">
-
-    <script src="js/libs/modernizr-2.5.3-respond-1.1.0.min.js"></script>
-    <script src="js/libs/jquery-1.7.2.min.js"></script>
 </head>
+
 
 
 <body>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          
-          <a class="brand" href="index.php">Web Site Template</a>
-          <div class="nav-collapse">
-            <ul class="nav">
-              <li><a href="#">Home</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
-    
-    <div class="container">
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+
+				<a class="brand" href="index.php">PixelOwner Template Website</a>
+				<div class="nav-collapse">
+					<ul class="nav">
+					<li><a href="#">Home</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
