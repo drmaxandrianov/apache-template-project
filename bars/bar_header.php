9ab7893e750a0f259e25012238733db682c94655
Maxim Pestun <max.pestun@gmail.com>
2013-02-17 23:07:01 +0400

    updated header title

--- bars/bar_header.php
+++ bars/bar_header.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>GMTool Extension</title>
+	<title>PixelOwner Template Website</title>
     <meta name="description" content="">
     <meta name="author" content="">
 
